fefd19c029e6f22d6ca4bca0ebda223dc28f5c59
Andreas Muck <andreas@muck.cl>
2024-06-09 18:00:42 -0400

    Change some examples

--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
 import search_combos
 
 values = "19157  13184  -1712  71397  17117  99999  77514  83002  -37856  96765  12298  -49214  -39541  -30826   33724  48876  -24915  4641  3255  -28090"
-targets =  "306538 242329"
-
-# search only combinations with 10 to 11 items
-search_combos.find(values, targets, i_range=range(10,12))
+targets =  "96765 306538 200000 42329"
 
 # search all possible combinations, including combined target sums
 search_combos.find(values, targets, i_combine_targets=True)
+
+# search only combinations with 3 to 6 items
+search_combos.find(values, targets, i_range=range(3, 7))
